34d39d52d393115882537309d633785856e44a0d
nokazn <41154684+nokazn@users.noreply.github.com>
2024-05-22 21:40:14 +0900

    feat: add some packages

---
+++ packages/components/Accordion/index.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Accordion } from './index.js';
+
+const meta = {
+  title: 'Accordion',
+  component: Accordion,
+  tags: ['autodocs'],
+  args: {},
+} satisfies Meta<typeof Accordion>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    summary: 'summary',
+    content: (
+      <>
+        <div
+          style={{
+            backgroundColor: 'grey',
+          }}
+        >
+          content
+        </div>
+      </>
+    ),
+  },
+};
---
+++ packages/hooks/useAsync/index.ts
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+type PromiseFunction<Succeeded> = (...parameters: never[]) => Promise<Succeeded>;
+
+type UseAsync<Succeeded, Failed, F extends PromiseFunction<Succeeded>> = {
+  data: Succeeded | null;
+  error: Failed | null;
+  execute: (...parameters: Parameters<F>) => void;
+  pending: boolean;
+};
+
+export const useAsync = <
+  F extends PromiseFunction<Succeeded>,
+  Succeeded extends Awaited<ReturnType<F>>,
+  Failed = unknown,
+>(
+  fetcher: F,
+): UseAsync<Awaited<ReturnType<F>>, Failed, F> => {
+  const [data, setData] = useState<Succeeded | null>(null);
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState<Failed | null>(null);
+
+  const execute = (...parameters: Parameters<F>) => {
+    setPending(true);
+    fetcher(...parameters)
+      .then(setData)
+      .catch(setError)
+      .finally(() => {
+        setPending(false);
+      });
+  };
+
+  return {
+    data,
+    error,
+    execute,
+    pending,
+  };
+};
